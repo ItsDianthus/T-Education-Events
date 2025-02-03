3219cd8ab5a197bb72ea5f22d09dad251460dc0d
Dianazavr <baileydia67@gmail.com>
2025-02-03 22:47:19 +0300

    Initial commit

---
+++ src/test/java/com/t_educational/t_edu_events/TEduEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t_educational.t_edu_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TEduEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
