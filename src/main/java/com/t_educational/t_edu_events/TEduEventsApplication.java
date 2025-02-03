84b7f681e9073dd3eb0673461224da7d826b1deb
Dianazavr <dianachanysheva@gmail.com>
2025-02-03 22:47:19 +0300

    Initial commit

---
+++ src/main/java/com/t_educational/t_edu_events/TEduEventsApplication.java
@@ -0,0 +1,13 @@
+package com.t_educational.t_edu_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TEduEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TEduEventsApplication.class, args);
+	}
+
+}
