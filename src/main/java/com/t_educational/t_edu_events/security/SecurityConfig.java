d6d3359e182b1b72b518db515b3902a5ee6516d6
Dianazavr <dianachanysheva@gmail.com>
2025-04-09 13:59:22 +0300

    swagger is working with auth

--- src/main/java/com/t_educational/t_edu_events/security/SecurityConfig.java
+++ src/main/java/com/t_educational/t_edu_events/security/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
